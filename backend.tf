bac41af04413040598145eb5fb310344ee4e59b6
11Hemant <hemantkhandave11@gmail.com>
2023-08-05 19:56:45 +0530

    changes in jenkins file

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "mydev-tf-state-bucket-9999"
     key = "main"
-    region = "us-east-2"
+    region = "ap-northeast-1"
     dynamodb_table = "my-dynamodb-table"
   }
 }
